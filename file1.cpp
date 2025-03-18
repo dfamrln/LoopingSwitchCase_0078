9a154865946694c6454c3f8e9879fdd22bb3a50f
dfamrln <mhdffa1@gmail.com>
2025-03-18 16:25:02 +0700

    Update file1.cpp

---
+++ file1.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    //perulangan for untuk pencacah naik
+    int i;
+    cout << "Perulangan For Pencacah Naik" << endl;
+    for (i = 0; i <= 4; i++){
+        cout << "Nilai i : " << i 
+        << " Lari keliling lapangan" << endl;
+    }
+    cout << "Nilai i terakhir : " << i << endl;
+    cout << endl;
